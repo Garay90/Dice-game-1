4f4108f2534d79ef4c0a20753bae2a4e1d0036df
garay90 <garay.90@mai.ru>
2020-10-12 21:32:40 +0300

    add second dice

--- dice_game.py
+++ dice_game.py
@@ -1,6 +1,7 @@
 
 import random
 from face_variation import dice_image
+from face_variation2 import dice_image2
 
 name = input("Input your name: ")
 print(" " + name.title() + " Let's play the dice game")
@@ -14,6 +15,10 @@ while moll:
     if dice == "r":
         x = random.randint(1,6)
         dice_image(x)
+        m = random.randint(1,6)
+        dice_image2(m)
+        a = str(x+m)
+        print("you have: " + a + " points\n")
 
     if dice == "q":
         moll = False
